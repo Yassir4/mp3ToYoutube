209835d6f6741843001631016a66728d367fe0f2
Yassir <hartaniyassir@gmail.com>
2018-07-30 08:45:29 +0100

    generate the video with the proper background

--- lib/Mp3ToYoutube.rb
+++ lib/Mp3ToYoutube.rb
@@ -7,9 +7,10 @@ module Mp3ToYoutube
     def upload mp3, img = false
       mp3_path = File.absolute_path mp3
       if File.exist? mp3_path
-        audioInfo = generate_audio_info mp3
+        audioInfo = generate_audio_info(mp3_path)
         video_background = generate_video_background(audioInfo)
-
+        video_file = generate_video_file(mp3_path, video_background)
+        upload_video_to_youtube video_file , audioInfo
       else
         raise "mp3 file not found"
       end
@@ -38,17 +39,24 @@ module Mp3ToYoutube
     end
 
 
-    # def generate_video_file mp3, video_background
-    #   video_file= File.basename(video_background, File.extname(video_background)) + ".jpg"
-    #   puts "generating videofile #{video_file} "
-    #   if !system( "ffmpeg", "-loop", "1", "-r", "2", "-i", "#{video_background}", "-i", "#{mp3}", "-vf", "scale=-1:1080", "-c:v", "libx264", "-preset", "slow", "-tune", "stillimage", "-crf", "18", "-c:a", "copy", "-shortest", "-pix_fmt", "yuv420p", "-threads", "0", "#{video_file}")
-    #     raise "generating videofile #{video_file} from #{mp3} and #{video_background} failed"
-    #   end
-    # end
-    # def upload_video_to_youtube
-    #   puts ""
-    #   video_title = `mediainfo --Inform='General;%Title% - %Artist%' #{audiofile}`.delete! "\n"
-    #   video_description = `mediainfo --Inform='General;%Album% - %Artist%' #{audiofile}`.delete! "\n"
-    # end
+    def generate_video_file mp3, video_background
+      video_file= File.basename(video_background, File.extname(video_background)) + ".avi"
+      puts "generating videofile #{video_file} "
+      if !system( "ffmpeg", "-loop", "1", "-r", "2", "-i", "#{video_background}", "-i", "#{mp3}", "-vf", "scale=-1:1080", "-c:v", "libx264", "-preset", "slow", "-tune", "stillimage", "-crf", "18", "-c:a", "copy", "-shortest", "-pix_fmt", "yuv420p", "-threads", "0", "#{video_file}")
+        raise "generating videofile #{video_file} from #{mp3} and #{video_background} failed"
+      end
+      return video_file
+    end
+
+
+    def upload_video_to_youtube video_file , audioInfo
+    puts "Uploading the video to Youtube"
+    video_title =  `mediainfo --Inform='General;%Title% - %Artist%' #{audioInfo}`.delete! "\n"
+    video_description = `mediainfo --Inform='General;%Album% - %Artist%' #{audioInfo}`.delete! "\n"
+    @account = Yt::Account.new refresh_token: "1/mO3TS_qEg1ETwS187y7h9pNHAoVtDlyhfMFZQHVD-ok"
+    @account.upload_video(videofile, privacy_status: 'public', title:  video_title, 
+                          description:  video_description)
+    puts "Video is uploaded successfully"
+    end
   end
 end
